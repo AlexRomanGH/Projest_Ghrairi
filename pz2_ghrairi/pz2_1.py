1d38705719e5e4378a8ee0e8aefaf34fb99074f5
AlexRomanGH <ghrairisamir5@gmail.com>
2025-10-18 15:10:04 +0300

    Update pz2_1.py

--- pz2_ghrairi/pz2_1.py
+++ pz2_ghrairi/pz2_1.py
@@ -7,7 +7,7 @@ except ValueError:
     
 try:
     summa = num // 10 + num % 10
-    proz = num // 10 * num % 10
+    proz = (num // 10) * (num % 10)
 
     print(f"Сумма цифр: {summa}\nПроизведение чисел: {proz}")
 except NameError:
